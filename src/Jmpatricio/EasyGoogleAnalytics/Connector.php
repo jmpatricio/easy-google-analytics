a8e1fdbff83327b604aa94be650abdb527a71990
João Patrício <jmpatricio@gmail.com>
2015-08-21 11:26:50 +0100

    Add new users metric. Added metric wrapper to easy get metric values

--- src/Jmpatricio/EasyGoogleAnalytics/Connector.php
+++ src/Jmpatricio/EasyGoogleAnalytics/Connector.php
@@ -15,24 +15,10 @@ use Google_Service_Analytics;
  */
 class Connector
 {
-
     /**
      * @var \Google_Service_Analytics
      */
     protected $analytics;
-
-    /**
-     * Get a package configuration value
-     *
-     * @param string $key The config key
-     *
-     * @return mixed The config value
-     * @since 1.0
-     */
-    protected function getConfig($key)
-    {
-        return \Config::get(EasyGoogleAnalyticsServiceProvider::PKG_NAME . '::' . $key);
-    }
 
     /**
      * Construct the client and analytics object
@@ -60,11 +46,92 @@ class Connector
     }
 
     /**
+     * Get a package configuration value
+     *
+     * @access protected
+     *
+     * @param string $key The config key
+     *
+     * @return mixed The config value
+     * @since  1.0
+     */
+    protected function getConfig($key)
+    {
+        return \Config::get(EasyGoogleAnalyticsServiceProvider::PKG_NAME . '::' . $key);
+    }
+
+    /**
+     * Get the default from date
+     *
+     * @access protected
+     *
+     * @param \Carbon\Carbon $fromDate The date
+     *
+     * @return \Carbon\Carbon
+     * @since  1.0
+     */
+    protected function getFromDateDefault(Carbon $fromDate = null)
+    {
+        if ( !$fromDate ) {
+            $fromDate = new Carbon('today');
+
+            return $fromDate;
+        }
+
+        return $fromDate;
+    }
+
+    /**
+     * Get the default to date
+     *
+     * @access protected
+     *
+     * @param \Carbon\Carbon $toDate The date
+     *
+     * @return \Carbon\Carbon
+     * @since  1.0
+     */
+    protected function getToDefaultDate(Carbon $toDate = null)
+    {
+        if ( !$toDate ) {
+            $toDate = new Carbon('today');
+            $toDate->hour(23)->minute(59)->second(59);
+
+            return $toDate;
+        }
+
+        return $toDate;
+    }
+
+    /**
+     * Wrapper to get a simple metric
+     *
+     * @access protected
+     *
+     * @param \Carbon\Carbon|null $fromDate The carbon object to from limit
+     * @param \Carbon\Carbon|null $toDate   The carbon object to to limit
+     * @param string              $metric   Metric to request
+     *
+     * @return mixed|null
+     *
+     * @since  1.0.3
+     */
+    protected function getGAMetric(Carbon $fromDate = null, Carbon $toDate = null, $metric)
+    {
+        $results = $this->getGA($fromDate, $toDate, $metric);
+
+        return (isset($results['totalsForAllResults'][$metric]))
+            ? (int)$results['totalsForAllResults'][$metric]
+            : null;
+    }
+
+    /**
      * Get the active users
      *
+     * @access public
      * @return int|null The number of active user by real time api
      *
-     * @since 1.0
+     * @since  1.0
      *
      */
     public function getActiveUsers()
@@ -79,30 +146,44 @@ class Connector
     /**
      * Get the total visit in time. Default is the current day
      *
+     * @access public
+     *
      * @param \Carbon\Carbon|null $fromDate The carbon object to from limit
      * @param \Carbon\Carbon|null $toDate   The carbon object to to limit
      *
      * @return int|null The total visits or null
-     * @since 1.0
+     * @since  1.0
      */
     public function getTotalVisits(Carbon $fromDate = null, Carbon $toDate = null)
     {
-        $fromDate = $this->getFromDateDefault($fromDate);
+        $metricResult = $this->getGAMetric($fromDate, $toDate, 'ga:visits');
 
-        $toDate = $this->getToDefaultDate($toDate);
-
-        $results = $this->analytics->data_ga->get($this->analyticsIds, $fromDate->format('Y-m-d'),
-            $toDate->format('Y-m-d'),
-            'ga:visits');
-
-        return (isset($results['totalsForAllResults']['ga:visits']))
-            ? (int)$results['totalsForAllResults']['ga:visits']
-            : null;
+        return ($metricResult) ? (int)$metricResult : null;
     }
 
+    /**
+     * Get the new users total
+     *
+     * @access public
+     *
+     * @param \Carbon\Carbon|null $fromDate The carbon object to from limit
+     * @param \Carbon\Carbon|null $toDate   The carbon object to to limit
+     *
+     * @return int|null The number of new users. Null otherwise
+     *
+     * @since  1.0.3
+     */
+    public function getTotalNewUsers(Carbon $fromDate = null, Carbon $toDate = null)
+    {
+        $metricResult = $this->getGAMetric($fromDate, $toDate, 'ga:newUsers');
+
+        return ($metricResult) ? (int)$metricResult : null;
+    }
 
     /**
      * Make a google analytics api request. Default is today
+     *
+     * @access public
      *
      * @param \Carbon\Carbon|null $fromDate The carbon object to from limit
      * @param \Carbon\Carbon|null $toDate   The carbon object to to limit
@@ -110,22 +191,42 @@ class Connector
      * @param array               $options  The metric options, like dimensions, sorting, filters
      *
      * @return \Google_Service_Analytics_GaData|null
-     * @since 1.0
+     * @since  1.0
      */
     public function getGA(Carbon $fromDate = null, Carbon $toDate = null, $metrics = null, $options = [])
     {
-        if (!$metrics) {
+        if ( !$metrics ) {
             return null;
         }
 
-        if (!$fromDate) {
-            $fromDate = new Carbon('today');
+        $fromDate = $this->getFromDateDefault($fromDate);
+        $toDate = $this->getToDefaultDate($toDate);
+
+        return $this->analytics->data_ga->get($this->analyticsIds, $fromDate->format('Y-m-d'), $toDate->format('Y-m-d'),
+            $metrics, $options);
+    }
+
+    /**
+     * Make a MFC api request
+     *
+     * @access public
+     *
+     * @param \Carbon\Carbon|null $fromDate The carbon object to from limit
+     * @param \Carbon\Carbon|null $toDate   The carbon object to to limit
+     * @param null                $metrics  Requested metrics
+     * @param array               $options  The metric options, like dimensions, sorting, filters
+     *
+     * @return \Google_Service_Analytics_GaData|null
+     * @since  1.0
+     */
+    public function getMCF(Carbon $fromDate = null, Carbon $toDate = null, $metrics = null, $options = [])
+    {
+        if ( !$metrics ) {
+            return null;
         }
 
-        if (!$toDate) {
-            $toDate = new Carbon('today');
-            $toDate->hour(23)->minute(59)->second(59);
-        }
+        $fromDate = $this->getFromDateDefault($fromDate);
+        $toDate = $this->getToDefaultDate($toDate);
 
         return $this->analytics->data_ga->get($this->analyticsIds, $fromDate->format('Y-m-d'), $toDate->format('Y-m-d'),
             $metrics, $options);
@@ -134,77 +235,16 @@ class Connector
     /**
      * Make a realtime api request
      *
+     * @access public
+     *
      * @param string $metrics Requested metrics
      * @param array  $options The metric options, like dimensions, sorting, filters
      *
      * @return \Google_Service_Analytics_RealtimeData
-     * @since
+     * @since  1.0
      */
     public function getRT($metrics, $options = [])
     {
         return $this->analytics->data_realtime->get($this->analyticsIds, $metrics, $options);
     }
-
-    /**
-     * Make a MFC api request
-     *
-     * @param \Carbon\Carbon|null $fromDate The carbon object to from limit
-     * @param \Carbon\Carbon|null $toDate   The carbon object to to limit
-     * @param null                $metrics  Requested metrics
-     * @param array               $options  The metric options, like dimensions, sorting, filters
-     *
-     * @return \Google_Service_Analytics_GaData|null
-     * @since 1.0
-     */
-    public function getMCF(Carbon $fromDate = null, Carbon $toDate = null, $metrics = null, $options = [])
-    {
-        if (!$metrics) {
-            return null;
-        }
-
-        $fromDate = $this->getFromDateDefault($fromDate);
-        $toDate = $this->getToDefaultDate($toDate);
-
-        return $this->analytics->data_ga->get($this->analyticsIds, $fromDate->format('Y-m-d'), $toDate->format('Y-m-d'),
-            $metrics, $options);
-    }
-
-    /**
-     * Get the default from date
-     * @param \Carbon\Carbon $fromDate The date
-     *
-     * @return \Carbon\Carbon
-     * @since 1.0
-     */
-    protected function getFromDateDefault(Carbon $fromDate = null)
-    {
-        if (!$fromDate) {
-            $fromDate = new Carbon('today');
-
-            return $fromDate;
-        }
-
-        return $fromDate;
-    }
-
-    /**
-     * Get the default to date
-     * @param \Carbon\Carbon $toDate The date
-     *
-     * @return \Carbon\Carbon
-     * @since 1.0
-     */
-    protected function getToDefaultDate(Carbon $toDate = null)
-    {
-        if (!$toDate) {
-            $toDate = new Carbon('today');
-            $toDate->hour(23)->minute(59)->second(59);
-
-            return $toDate;
-        }
-
-        return $toDate;
-    }
-
-
 }
